cbdd658d12b9a91c3de622e01644765d84c8eadf
Simon Brenner <sbrenner@caa.tuwien.ac.at>
2021-11-23 11:50:08 +0100

    bug fixed in jcamp parsing

--- spectra-graphs/spectra-graph.js
+++ spectra-graphs/spectra-graph.js
@@ -1,27 +1,28 @@
-
-
 function showSpectrum(url, target_element_id) {
   fetch(url, {mode: 'cors'}).then((r)=>{r.text().then((d)=>{
     var jcampLines = d.split(/\r\n|\n/);
     var spectralData = [];
     var xunits = '';
     var yunits = '';
-    jcampLines.forEach((line) => {
-      elems = line.split(' ');
-      if (elems.length == 2){
+    jcampLines.some((line) => {
+      elems = line.split(/ (.+)/);
+      if (elems.length >= 2){
         if (elems[0] == '##XUNITS=')
           xunits = elems[1];
         else if (elems[0] == '##YUNITS=')
           yunits = elems[1];
       }
+      return xunits != '' && yunits != '';
     });
     jcampLines.forEach((line) => {
-      elems = line.split(' ');
-      if (!isNaN(elems[0]) && !isNaN(elems[1])){
-        var point = {};
-        point[xunits] = Number(elems[0]);
-        point[yunits] = Number(elems[1]);
-        spectralData.push(point);
+      elems = line.split(/ (.+)/);
+      if (elems.length >= 2){
+        if (!isNaN(elems[0]) && !isNaN(elems[1])){
+          var point = {};
+          point[xunits] = Number(elems[0]);
+          point[yunits] = Number(elems[1]);
+          spectralData.push(point);
+        }
       }
     });
 
