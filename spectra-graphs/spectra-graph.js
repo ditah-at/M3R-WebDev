542edeb4ee51ed171d5dd2cb3c66671808f1355b
Simon Brenner <sbrenner@caa.tuwien.ac.at>
2021-11-23 13:02:37 +0100

    plot style, reverse x axis for negative deltax

--- spectra-graphs/spectra-graph.js
+++ spectra-graphs/spectra-graph.js
@@ -1,9 +1,12 @@
 function showSpectrum(url, target_element_id) {
   fetch(url, {mode: 'cors'}).then((r)=>{r.text().then((d)=>{
+    // parse jcamp file
     var jcampLines = d.split(/\r\n|\n/);
     var spectralData = [];
     var xunits = '';
     var yunits = '';
+    var deltax = 0;
+    // first pass: get metadata
     jcampLines.some((line) => {
       elems = line.split(/ (.+)/);
       if (elems.length >= 2){
@@ -11,9 +14,12 @@ function showSpectrum(url, target_element_id) {
           xunits = elems[1];
         else if (elems[0] == '##YUNITS=')
           yunits = elems[1];
+        else if (elems[0] == '##DELTAX=')
+          deltax = Number(elems[1]);
       }
-      return xunits != '' && yunits != '';
+      return xunits != '' && yunits != '' && deltax !=0;  //if we have read these values, we are done.
     });
+    // second pass: read spectrum (needs some of the metadata)
     jcampLines.forEach((line) => {
       elems = line.split(/ (.+)/);
       if (elems.length >= 2){
@@ -26,23 +32,35 @@ function showSpectrum(url, target_element_id) {
       }
     });
 
-    //var sorted = spectralData.sort(function(a, b) {return a.xunit - b.yunit;});
+    // sort values. perhaps this is not necessary, but let's be sure.
+    // if (deltax > 0)
+    //   spectralData = spectralData.sort(function(a, b) {return a.xunit - b.xunit;});
+    // if (deltax < 0)
+    //   spectralData = spectralData.sort(function(a, b) {return b.xunit - a.xunit;});
+    
+    //make the plot
     var plot  = Plot.plot({
-      width: 500,
-      height: 300,
       y: {
         grid: true
+      },
+      x: {
+        reverse: deltax<0
       },
       marks: [
         Plot.line(spectralData, {
           x: xunits,
-          y: yunits
+          y: yunits,
+          stroke: '#885457',
+          sort: xunits
         }),
-      ],
+      ]
     });
     
+    //insert into DOM
+    //version 1: replace an element
     plot.id = target_element_id;
     document.getElementById(target_element_id).replaceWith(plot);
+    //version 2: append a child to an element
     //document.getElementById(parent_id).appendChild(plot);
 
   }, reason => {console.log(reason)})
